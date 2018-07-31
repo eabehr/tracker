79d5c2139c9a207ad5a4a6f32056dcbcf5196f8e
eabehr <emilyabehrendt@gmail.com>
2018-07-30 22:05:00 -0700

    add new fields to todoservice

--- client/src/components/TodoService.js
+++ client/src/components/TodoService.js
@@ -4,7 +4,9 @@ class TodoService {
 
     sendData(data) {
         axios.post('http://localhost:4200/todos/add/post', {
-            title: data.title
+            title: data.title,
+            cadence: data.cadence,
+            frequency: data.frequency
         })
         .then(function (response) {
            console.log(response);
